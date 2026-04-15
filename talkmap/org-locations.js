fee092df6e472afac0d752071991c647e4af76d1
Parmenion Mavrikakis <parmenion.mavrikakis@epfl.ch>
2026-04-15 14:32:39 +0200

    New talks' locations.

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,23 +1,13 @@
 var addressPoints = [
   [
-    "Modeling Reconfigurable Intelligent Surfaces with the Finite Element Method<br />PhotonIcs & Electromagnetics Research Symposium (PIERS); Abu Dhabi, United Arab Emirates",
-    24.4538352,
-    54.3774014
-  ],
-  [
-    "Modeling Light Scattering in Layered Media with the Surface Integral Equation Method<br />International Applied Computational Electromagnetics Society (ACES) Symposium; Orlando, United States of America",
-    28.5421109,
-    -81.3790304
-  ],
-  [
     "Self-Consistent Modeling of Coupled Maxwell-Rate Equations with the Finite-Difference Time-Domain Method<br />31st International Workshop on Optical Wave & Waveguide Theory and Numerical Modelling (OWTNM); Nottingham, United Kingdom",
     52.9534193,
     -1.1496461
   ],
   [
-    "Modeling of plasmonic, dielectric and hybrid nanostructures with surface integral equations<br />30th International Workshop on Optical Wave & Waveguide Theory and Numerical Modelling (OWTNM); Kiel, Germany",
-    54.3227085,
-    10.135555
+    "Modeling of metasurfaces<br />3rd Colloquium on the Physics and Applications of Metasurfaces; Z\u00fcrich, Switzerland",
+    47.3744489,
+    8.5410422
   ],
   [
     "Modeling Complex Atomic Structures with the Surface Integral Equation Method<br />PhotonIcs & Electromagnetics Research Symposium (PIERS); Abu Dhabi, United Arab Emirates",
@@ -25,9 +15,24 @@ var addressPoints = [
     54.3774014
   ],
   [
-    "Modeling of metasurfaces<br />3rd Colloquium on the Physics and Applications of Metasurfaces; Z\u00fcrich, Switzerland",
-    47.3744489,
-    8.5410422
+    "Modeling Light Scattering in Layered Media with the Surface Integral Equation Method<br />International Applied Computational Electromagnetics Society (ACES) Symposium; Orlando, United States of America",
+    28.5421218,
+    -81.379045
+  ],
+  [
+    "Modeling Reconfigurable Intelligent Surfaces with the Finite Element Method<br />PhotonIcs & Electromagnetics Research Symposium (PIERS); Abu Dhabi, United Arab Emirates",
+    24.4538352,
+    54.3774014
+  ],
+  [
+    "A surface integral equation framework for light scattering in layered media<br />32nd International Workshop on Optical Wave & Waveguide Theory and Numerical Modelling (OWTNM); EPFL, Lausanne, Switzerland",
+    46.5172426,
+    6.561216
+  ],
+  [
+    "Modeling of plasmonic, dielectric and hybrid nanostructures with surface integral equations<br />30th International Workshop on Optical Wave & Waveguide Theory and Numerical Modelling (OWTNM); Kiel, Germany",
+    54.3227085,
+    10.135555
   ],
   [
     "Self-consistent Modeling of Coupled Maxwell-rate Equations with the Finite-Difference Time-Domain Method<br />PhotonIcs & Electromagnetics Research Symposium (PIERS); Abu Dhabi, United Arab Emirates",
